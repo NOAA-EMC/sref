1782f17883e18c4bd1a9d3b1beee99c6615a7f62
dusan.jovic@noaa.gov <dusan.jovic@noaa.gov>
2017-09-28 16:18:23 +0000

    fix gpfs paths

--- run/run_wcoss_cray.sh
+++ run/run_wcoss_cray.sh
@@ -2,27 +2,27 @@
 
 set -eux
 
-FV3HREFDIR=/gpfs/hps/emc/meso/save/${LOGNAME}/fv3refs
+FV3HREFDIR=/gpfs/hps3/emc/meso/save/${LOGNAME}/fv3refs
 cd $FV3HREFDIR/run
 
 cyc=$1
 FLENGTH=48
 ##ymdh=`cat /gpfs/gp2/nco/ops/com/date/t${cyc}z | cut -c7-16`
 
-ymdh=20170914${cyc}
+ymdh=20170928${cyc}
 
 PDY=`echo $ymdh | cut -c1-8`
 CYC=`echo $ymdh | cut -c9-10`
 
-rm -rf /gpfs/hps/ptmp/$LOGNAME/fv3href
-mkdir -p /gpfs/hps/ptmp/$LOGNAME/fv3href
+rm -rf /gpfs/hps3/ptmp/$LOGNAME/fv3href
+mkdir -p /gpfs/hps3/ptmp/$LOGNAME/fv3href
 rm -f *.bsub *.log
 
 export MACHINE=wcoss_cray
 
 #for MEMBER in ctl mem1 mem2 mem3 mem4 mem5 mem6 mem7 mem8 mem9 ; do
-for MEMBER in ctl mem1 mem2 ; do
-#for MEMBER in ctl ; do
+#for MEMBER in ctl mem1 mem2 ; do
+for MEMBER in ctl ; do
 
 cat FV3HREF_PREP.bsub.in | \
     sed s:_PDY_:$PDY:g | \
@@ -42,7 +42,7 @@ cat FV3HREF_FCST.bsub.in | \
 bsub                                       < FV3HREF_PREP_${MEMBER}.bsub
 bsub -w "done(\"FV3HREF_PREP_${MEMBER}\")" < FV3HREF_FCST_${MEMBER}.bsub
 
-echo "FV3HREF waiting ${MEMBER} PREP" >> /gpfs/hps/ptmp/$LOGNAME/fv3href/jlogfile_sref
-echo "FV3HREF waiting ${MEMBER} FCST" >> /gpfs/hps/ptmp/$LOGNAME/fv3href/jlogfile_sref
+echo "FV3HREF waiting ${MEMBER} PREP" >> /gpfs/hps3/ptmp/$LOGNAME/fv3href/jlogfile_sref
+echo "FV3HREF waiting ${MEMBER} FCST" >> /gpfs/hps3/ptmp/$LOGNAME/fv3href/jlogfile_sref
 
 done
