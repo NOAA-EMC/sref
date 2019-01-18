eada6cb0bf5b3944314bac7b6caa33634852d0eb
jun.du@noaa.gov <jun.du@noaa.gov>
2019-01-18 18:27:16 +0000

    add George's changes in coldstart

--- sorc/sref_coldstart.fd/make.sh
+++ sorc/sref_coldstart.fd/make.sh
@@ -3,9 +3,7 @@
 #---------------------------------------------------------------------------------
 #  The driver script for compiling the emcsfc_coldstart program.  Loads
 #  module files and exports environment variables required by the makefile
-#  Then, invokes the makefile.  
-#
-#  Only tested on Zeus and the NCEP WCOSS machines.
+#  Then, invokes the makefile.  Only works on WCOSS Dell.
 #
 #  To invoke: type 'make.sh' from the command line.  If successfully built, 
 #  the executable will be installed the ../../exec subdirectory.
@@ -17,76 +15,34 @@
 
 mac=$(hostname | cut -c1-1)
 
-if [ $mac = t -o $mac = g ] ; then  #For WCOSS
+if [ $mac = v -o $mac = m ] ; then  # WCOSS Dell
 
   echo
-  echo "BUILD EMCSFC_COLDSTART PROGRAM ON WCOSS"
+  echo "BUILD EMCSFC_COLDSTART PROGRAM ON WCOSS DELL."
   echo
 
   module purge
-
-# load intel compiler
-
-  module load ics/12.1
-  export FCOMP=mpfort
-  export FFLAGS="-O0 -r8 -i4 -FR -convert big_endian -compiler intel"
-  export FPPFLAGS="-fpp -DGFS=0 -save-temps"
-  export LFLAGS="-openmp"
-
-# load ncep library modules
-
+  module load EnvVars/1.0.2
+  module load ips/18.0.1.163
+  module load impi/18.0.1
   module load NetCDF/3.6.3
-  module load sfcio/v1.0.0
-  module load ip/v2.0.0
-  module load sp/v2.0.2
-  module load w3nco/v2.0.6
-  module load bacio/v2.0.1
-  module load jasper/v1.900.1
-  module load z/v1.2.6
-  module load png/v1.2.44
-  module load g2/v2.5.0
-  module load nemsio/v2.2.1
-  module load landsfcutil/v2.0.0
-
-# ensure libraries and include directories exist.
-
-  make check_prereqs
-  rc=$?
-  if ((rc != 0));then
-    echo "MISSING LIBRARY AND/OR INCLUDE DIRECTORY. EXIT."
-    exit
-  fi
-
-elif [ $mac = f ] ; then  #For Zeus
-
-  echo
-  echo "BUILD EMCSFC_COLDSTART PROGRAM ON ZEUS"
-  echo
-
-  module purge
-  module load mpt/2.06
-  module load intel/12-12.0.4.191
-  export FCOMP=ifort
-  export FFLAGS="-O0 -convert big_endian -r8 -i4 -FR"
-  export FPPFLAGS="-fpp -DGFS=0 -save-temps"
-  export LFLAGS="-openmp -lmpi"
-
-# load ncep library modules
-
-  module load netcdf/3.6.3-intel
-  export NETCDF_INCLUDE=-I${NETCDF}/include
-  export NETCDF_LDFLAGS_F="-L${NETCDF}/lib -lnetcdf"
-  module load sfcio/v1.1.0
-  module load ip/v2.0.0
-  module load sp/v2.0.2
-  module load w3nco/v2.0.6
-  module load bacio/v2.0.1
+  module load sfcio/1.0.0
+  module load ip/3.0.1
+  module load sp/2.0.2
+  module load w3nco/2.0.6
+  module load bacio/2.0.2
   module load jasper/1.900.1
-  module load z/1.2.6
-  module load png/12
-  module load g2/v2.5.0
-  module load nemsio/v2.2.1
-  module load landsfcutil/v2.0.0
+  module load zlib/1.2.11
+  module load libpng/1.2.44
+  module load g2/3.1.0
+  module load nemsio/2.2.3
+  module load landsfcutil/2.1.0
+  module list
+
+  export FCOMP=mpif90
+  export FFLAGS="-O0 -r8 -i4 -FR -convert big_endian"
+  export FPPFLAGS="-fpp -DGFS=0 -save-temps"
+  export LFLAGS="-qopenmp"
 
 else
 
