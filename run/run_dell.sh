4f4202b5c834b75b092db4b6e5800b3ac399aa4c
dusan.jovic@noaa.gov <dusan.jovic@noaa.gov>
2019-02-05 22:41:39 +0000

    port jobs and scripts for wrf fcst step

--- run/run_dell.sh
+++ run/run_dell.sh
@@ -10,7 +10,7 @@ FLENGTH=87
 INCR=3
 ymdh=`cat /com/date/t${cyc}z | cut -c7-16`
 
-ymdh=20190204${cyc}
+ymdh=20190205${cyc}
 
 export SMSBIN=${HOME}/sms
 export MACHINE=dell
@@ -26,14 +26,29 @@ rm -rf /gpfs/dell2/ptmp/$LOGNAME/tmpnwprd/jlogfile_sref
 
 rm -f *.bsub *.log
 
-#for MODEL in ARW NMB ; do
-for MODEL in ARW ; do
+declare -A NMMB_FCST_TASKS
+NMMB_FCST_TASKS[ctl]=112
+NMMB_FCST_TASKS[n01]=112
+NMMB_FCST_TASKS[n02]=96
+NMMB_FCST_TASKS[n03]=96
+NMMB_FCST_TASKS[n04]=128
+NMMB_FCST_TASKS[n05]=128
+NMMB_FCST_TASKS[n06]=96
+NMMB_FCST_TASKS[p01]=112
+NMMB_FCST_TASKS[p02]=128
+NMMB_FCST_TASKS[p03]=128
+NMMB_FCST_TASKS[p04]=96
+NMMB_FCST_TASKS[p05]=112
+NMMB_FCST_TASKS[p06]=96
+
+for MODEL in ARW NMB ; do
+#for MODEL in ARW ; do
 #for MODEL in NMB ; do
 #for MODEL in ARW RAW NMM NMB ; do
 
 #for MEMBER in ctl n01 p01 n02 p02 n03 p03 n04 p04 n05 p05 n06 p06; do
+#for MEMBER in ctl; do
 for MEMBER in ctl n01 p01 n02 p02 n03 p03 n04 p04; do
-#for MEMBER in ctl n01 p01 n02 p02 n03 p03 n04 p04; do
 
 cat SREF_PREP.bsub.in | \
     sed s:_PDY_:$PDY:g | \
@@ -66,9 +81,9 @@ cat SREF_REAL.bsub.in | \
     sed s:_RES_:$RES:g | \
     sed s:_MACHINE_:$MACHINE:g > SREF_REAL_${MODEL}_${MEMBER}.bsub
 
-if [ 1 == 0 ]; then
 
 cat SREF_FCST.bsub.in | \
+    sed s:_TASKS_:${NMMB_FCST_TASKS[$MEMBER]}:g | \
     sed s:_PDY_:$PDY:g | \
     sed s:_CYC_:$CYC:g | \
     sed s:_FLENGTH_:$FLENGTH:g | \
@@ -79,6 +94,8 @@ cat SREF_FCST.bsub.in | \
     sed s:_IOFORM_:$IOFORM:g | \
     sed s:_RES_:$RES:g | \
     sed s:_MACHINE_:$MACHINE:g > SREF_FCST_${MODEL}_${MEMBER}.bsub
+
+if [ 0 == 1 ]; then
 
 # 3 hourly 212, 216, 234 grids
 export runflag=3hrly
@@ -148,16 +165,15 @@ cat SREF_POST4.bsub.in | \
     sed s:_runflag_:$runflag:g | \
     sed s:_OUTGRD_:$OUTGRD:g > SREF_POST4_${MODEL}_${MEMBER}.bsub
 
+fi
 
 #if [ ${MEMBER} == ctl ]; then
 #CTLJOB=`bsub SREF_PREP_${MODEL}_${MEMBER}.bsub`
-#echo "SREF waiting ${MODEL} ${MEMBER} PREP" >> /gpfs/dell2/ptmp/Dusan.Jovic/tmpnwprd/jlogfile_sref
+#echo "SREF waiting ${MODEL} ${MEMBER} PREP" >> /gpfs/dell2/ptmp/$LOGNAME/tmpnwprd/jlogfile_sref
 #else
 #bsub -W depend=afterok:$CTLJOB SREF_PREP_${MODEL}_${MEMBER}.bsub
-#echo "SREF waiting ${MODEL} ${MEMBER} PREP" >> /gpfs/dell2/ptmp/Dusan.Jovic/tmpnwprd/jlogfile_sref
+#echo "SREF waiting ${MODEL} ${MEMBER} PREP" >> /gpfs/dell2/ptmp/$LOGNAME/tmpnwprd/jlogfile_sref
 #fi
-
-fi
 
 done
 
