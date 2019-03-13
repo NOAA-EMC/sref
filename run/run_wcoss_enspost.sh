d7f37002c1f349925e9da103c711b9add521c698
jun.du@noaa.gov <jun.du@noaa.gov>
2019-03-13 17:26:13 +0000

    ensemble post run scripts

--- run/run_wcoss_enspost.sh
+++ run/run_wcoss_enspost.sh
@@ -2,7 +2,7 @@
 
 set -aeux
 
-SREFDIR=/meso/save/${LOGNAME}/sref.v7.0.0
+SREFDIR=/gpfs/dell2/emc/modeling/noscrub/${LOGNAME}/sref.v7_dell
 cd $SREFDIR/run
 
 #type=1hrly
@@ -11,18 +11,18 @@ cd $SREFDIR/run
 #type=all
 
 cyc=$1
-ymdh=`cat /com/date/t${cyc}z | cut -c7-16`
+ymdh=`cat /gpfs/hps/nco/ops/com/date/t${cyc}z | cut -c7-16`
 
-ymdh=20150331${1}
+ymdh=20190311${1}
 
 export SMSBIN=${HOME}/sms
 
 PDY=`echo $ymdh | cut -c1-8`
 CYC=`echo $ymdh | cut -c9-10`
 
-#rm -rf /ptmp/$LOGNAME/sref
-#mkdir -p /ptmp/$LOGNAME/tmpnwprd
-#rm -rf /ptmp/$LOGNAME/tmpnwprd/jlogfile_sref
+#rm -rf /gpfs/dell2/ptmp/$LOGNAME/sref
+#mkdir -p /gpfs/dell2/ptmp/$LOGNAME/tmpnwprd
+#rm -rf /gpfs/dell2/ptmp/$LOGNAME/tmpnwprd/jlogfile_sref
 
 #rm -f *.bsub *.log
 
@@ -32,7 +32,7 @@ cat SREF_ENSPOST.bsub.in | \
     sed s:_CYC_:$CYC:g | \
     sed s:_SREFDIR_:$SREFDIR:g > SREF_ENSPOST.bsub
 bsub < SREF_ENSPOST.bsub
-echo "SREF waiting ENSPOST" >> /ptmpp1/$LOGNAME/tmpnwprd/jlogfile_sref
+echo "SREF waiting ENSPOST" >> /gpfs/dell2/ptmp/$LOGNAME/tmpnwprd/jlogfile_sref
 fi
 
 if [ $type = 1hrly -o $type = all ]; then
@@ -41,7 +41,7 @@ cat SREF_ENSPOST_1hrly.bsub.in | \
     sed s:_CYC_:$CYC:g | \
     sed s:_SREFDIR_:$SREFDIR:g > SREF_ENSPOST_1hrly.bsub
 bsub < SREF_ENSPOST_1hrly.bsub
-echo "SREF waiting ENSPOST_1hrly" >> /ptmpp1/$LOGNAME/tmpnwprd/jlogfile_sref
+echo "SREF waiting ENSPOST_1hrly" >> /gpfs/dell2/ptmp/$LOGNAME/tmpnwprd/jlogfile_sref
 fi
 
 if [ $type = 16km -o $type = all ]; then
@@ -55,7 +55,7 @@ cat SREF_ENSPOST132.bsub.in | \
     sed s:_ORDER_:$ORDER:g | \
     sed s:_SREFDIR_:$SREFDIR:g > SREF_ENSPOST132_$ORDER.bsub
 bsub < SREF_ENSPOST132_$ORDER.bsub
-echo "SREF waiting ENSPOST132_$ORDER" >> /ptmpp1/$LOGNAME/tmpnwprd/jlogfile_sref
+echo "SREF waiting ENSPOST132_$ORDER" >> /gpfs/dell2/ptmp/$LOGNAME/tmpnwprd/jlogfile_sref
 done
 fi
 
