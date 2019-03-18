416e0d4afde55b96304bc0a7706339eef21e2adf
jun.du@noaa.gov <jun.du@noaa.gov>
2019-03-18 17:18:58 +0000

    cluster job submission scripts

--- run/run_wcoss_cluster.sh
+++ run/run_wcoss_cluster.sh
@@ -2,22 +2,22 @@
 
 set -aeux
 
-SREFDIR=/meso/save/${LOGNAME}/sref.v7.0.0
+SREFDIR=/gpfs/dell2/emc/modeling/noscrub/${LOGNAME}/sref.v7_dell
 cd $SREFDIR/run
 
 cyc=$1
-ymdh=`cat /com/date/t${cyc}z | cut -c7-16`
+ymdh=`cat /gpfs/hps/nco/ops/com/date/t${cyc}z | cut -c7-16`
 
-ymdh=20150331${1}
+ymdh=20190318${1}
 
 export SMSBIN=${HOME}/sms
 
 PDY=`echo $ymdh | cut -c1-8`
 CYC=`echo $ymdh | cut -c9-10`
 
-#rm -rf /ptmpp1/$LOGNAME/sref
-#mkdir -p /ptmpp1/$LOGNAME/tmpnwprd
-#rm -rf /ptmpp1/$LOGNAME/tmpnwprd/jlogfile_sref
+#rm -rf /gpfs/dell2/ptmp/$LOGNAME/sref
+#mkdir -p /gpfs/dell2/ptmp/$LOGNAME/tmpnwprd
+#rm -rf /gpfs/dell2/ptmp/$LOGNAME/tmpnwprd/jlogfile_sref
 
 #rm -f *.bsub *.log
 
@@ -27,5 +27,5 @@ cat SREF_CLUSTER.bsub.in | \
     sed s:_SREFDIR_:$SREFDIR:g > SREF_CLUSTER.bsub
 
 bsub < SREF_CLUSTER.bsub
-echo "SREF waiting CLUSTER" >> /ptmpp1/$LOGNAME/tmpnwprd/jlogfile_sref
+echo "SREF waiting CLUSTER" >> /gpfs/dell2/ptmp/$LOGNAME/tmpnwprd/jlogfile_sref
 
