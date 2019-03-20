5e5330caa277de52bce925078597d44add0c2680
jun.du@noaa.gov <jun.du@noaa.gov>
2019-03-20 18:25:30 +0000

    bias corection submision scripts

--- run/run_wcoss_bias.sh
+++ run/run_wcoss_bias.sh
@@ -2,22 +2,22 @@
 
 set -aeux
 
-SREFDIR=/meso/save/${LOGNAME}/sref.v7.0.0
+SREFDIR=/gpfs/dell2/emc/modeling/noscrub/${LOGNAME}/sref.v7_dell
 cd $SREFDIR/run
 
 cyc=$1
-ymdh=`cat /com/date/t${cyc}z | cut -c7-16`
+ymdh=`cat /gpfs/hps/nco/ops/com/date/t${cyc}z | cut -c7-16`
 
-ymdh=20150331${1}
+ymdh=20190318${1}
 
 export SMSBIN=${HOME}/sms
 
 PDY=`echo $ymdh | cut -c1-8`
 CYC=`echo $ymdh | cut -c9-10`
 
-#rm -rf /ptmp/$LOGNAME/sref
-#mkdir -p /ptmp/$LOGNAME/tmpnwprd
-#rm -rf /ptmp/$LOGNAME/tmpnwprd/jlogfile_sref
+#rm -rf /gpfs/dell2/ptmp/$LOGNAME/sref
+#mkdir -p /gpfs/dell2/ptmp/$LOGNAME/tmpnwprd
+#rm -rf /gpfs/dell2/ptmp/$LOGNAME/tmpnwprd/jlogfile_sref
 
 #rm -f *.bsub *.log
 
@@ -36,11 +36,11 @@ cat SREF_QPFBIAS.bsub.in | \
     sed s:_CYC_:$CYC:g | \
     sed s:_SREFDIR_:$SREFDIR:g > SREF_QPFBIAS.bsub
 bsub < SREF_CALFCSTERR.bsub
-echo "SREF waiting CALFCSTERR" >> /ptmpp1/$LOGNAME/tmpnwprd/jlogfile_sref
+echo "SREF waiting CALFCSTERR" >> /gpfs/dell2/ptmp/$LOGNAME/tmpnwprd/jlogfile_sref
 
 #bsub < SREF_BIAS.bsub
-#echo "SREF waiting BIAS" >> /ptmp/$LOGNAME/tmpnwprd/jlogfile_sref
+#echo "SREF waiting BIAS" >> /gpfs/dell2/ptmp/$LOGNAME/tmpnwprd/jlogfile_sref
 
 #bsub < SREF_QPFBIAS.bsub
-#echo "SREF waiting QPFBIAS" >> /ptmp/$LOGNAME/tmpnwprd/jlogfile_sref
+#echo "SREF waiting QPFBIAS" >> /gpfs/dell2/ptmp/$LOGNAME/tmpnwprd/jlogfile_sref
 
